19e676b641aea825296a2280e3f9c68362170e71
markfour <axhfaro@gmail.com>
2020-08-04 01:25:49 +0900

    Add query

--- TODO/ViewController.swift
+++ TODO/ViewController.swift
@@ -8,13 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+import Amplify
+import AmplifyPlugins
 
+class ViewController: UIViewController {
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    
+    performOnAppear()
   }
-
-
+  
+  func performOnAppear() {
+    let item = Todo(name: "Finish quarterly taxes",
+                    priority: .high,
+                    description: "Taxes are due for the quarter next week")
+    save(item: item)
+  }
+  
+  func save(item: Todo) {
+    Amplify.DataStore.query(Todo.self, completion: { result in
+      switch(result) {
+      case .success(let todos):
+        for todo in todos {
+          print("==== Todo ====")
+          print("Name: \(todo.name)")
+          if let priority = todo.priority {
+            print("Priority: \(priority)")
+          }
+          if let description = todo.description {
+            print("Description: \(description)")
+          }
+        }
+      case .failure(let error):
+        print("Could not query DataStore: \(error)")
+      }
+    })
+  }
 }
 
